29257ef49352d434212c9622b7ac90825027745e
dylanlu <dylanlu@dylans-mbp-2.crescentschool.local>
2018-01-31 22:19:15 -0500

    Fixed buttons. Still no on click.

--- src/application/Battleship.java
+++ src/application/Battleship.java
@@ -1,29 +1,53 @@
 package application;
 
+import java.util.Random;
+
 import javafx.scene.control.Button;
 import javafx.scene.layout.GridPane;
 
 public class Battleship implements Grid {
 	int turn = 0;
 	Button[][] grid = new Button[5][5];
+	GridPane Battleboard = new GridPane();
 
 	public Battleship() {
 	}
 
 	public GridPane setupBoard() {
 
-		GridPane BattleBoard = new GridPane();
 		for (int i = 0; i < 5; i++) {
 			for (int a = 0; a < 5; a++) {
 				grid[i][a] = new Button();
 				grid[i][a].setMaxSize(150, 150);
-				grid[i][a].setTranslateX(-300 + i * 150);
-				grid[i][a].setTranslateY(-300 + a * 150);
-				BattleBoard.getChildren().add(grid[i][a]);
+				Battleboard.add(grid[i][a], i, a);
 			}
 		}
+		Random random = new Random();
+		int x = random.nextInt();
 
-		return BattleBoard;
+		setOnClickEvents();
+		return Battleboard;
+	}
+
+	int k = 0;
+	int j = 0;
+
+	private void setOnClickEvents() {
+		System.out.println("Onclicked");
+		for (; k < 5; k++) {
+			for (; j < 5; j++) {
+				grid[k][j].setOnAction(value -> {
+					System.out.println("Button clicked");
+					removeButton(k, j);
+				});
+			}
+		}
+	}
+
+	private void removeButton(int k, int j) {
+		grid[k][j].setStyle("fx-background-color: #ff7c7c");
+		grid[k][j].setOnAction(value -> {
+		});
 	}
 
 	@Override

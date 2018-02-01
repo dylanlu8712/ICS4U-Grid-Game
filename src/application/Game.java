b346247f89ad2f1ea44ab77c2ae90905973e9c7b
dylanlu8712 <dylanlu@crescentschool.org>
2018-01-31 23:30:00 -0500

    Added comments.

--- src/application/Game.java
+++ src/application/Game.java
@@ -1,7 +1,7 @@
 package application;
 
 public abstract class Game {
-	//Code that is used by all classes.
+	//Code that is used by most classes.
 	static int turn = 0;
 	public abstract int switchTurn();
 }
